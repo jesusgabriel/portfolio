889ce8a52fcf68cfb8c922902ee72fc1366f18dd
Andy Melo <amelo@fvi.edu>
2016-09-27 22:32:57 -0400

    save

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    var $menu = $("header nav");
+    var $open = $("#menu-open");
+    var $close = $("#menu-close");
+
+    if (document.documentElement.clientWidth < 678) {
+
+        $open.on("click", function() {
+            $menu.fadeIn();
+            $menu.css("left", "0px");
+            $menu.css("box-shadow", "0 0 20px rgba(0, 0, 0, .7 )");
+        });
+
+        $("#menu-close, header nav ul li a").on("click", function() {
+            $menu.fadeOut();
+            $menu.css("left", "-250px");
+            $menu.css("box-shadow", "none");
+        });
+
+    }
+
+
+});
